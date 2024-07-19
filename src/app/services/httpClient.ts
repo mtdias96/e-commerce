cb8df94435707d746e74e58c524a133d79fef451
mtdias96 <matheusdias.front96@gmail.com>
2024-07-19 11:22:22 -0300

    feature: alteração do baseURL pordução

--- src/app/services/httpClient.ts
+++ src/app/services/httpClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const httpClient = axios.create({
   // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://ecommerce-api-jo6k.onrender.com'
 })
 
 
