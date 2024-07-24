090a0ff033016d3ddba69c27f4f714f4419e812a
mtdias96 <matheusdias.front96@gmail.com>
2024-07-24 17:06:06 -0300

    feature: skeleton page productList

--- src/app/services/httpClient.ts
+++ src/app/services/httpClient.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
-import { sleep } from "../utils/sleep";
 
 export const httpClient = axios.create({
   baseURL: 'https://ecommerce-api-jo6k.onrender.com'
 })
 
-httpClient.interceptors.response.use(async data => {
-  await sleep(1500)
+// httpClient.interceptors.response.use(async data => {
+//   await sleep(1500)
 
-  return data
-})
+//   return data
+// })
 
 export default httpClient
--- src/view/pages/ProductList/index.tsx
+++ src/view/pages/ProductList/index.tsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import { useFilter } from "../../../app/hooks/useFilter";
 import { CardProduct } from "../../components/CardProduct";
 import Menu from "../../components/Menu";
+import { SkeletonCard } from "../../components/SkeletonCard";
 import { AsideMenu } from "./components/AsideMenu";
 import { useProductListController } from "./useProductListController";
 
 export function ProductList() {
-  const { products } = useProductListController();
+  const { products, isPending } = useProductListController();
   const { productFilter } = useFilter();
   const [quantity, setQuantity] = useState(0)
+  const skeletonCards = [1, 2, 3, 4, 5, 6, 7, 8]
 
 
   useEffect(() => {
@@ -26,7 +28,7 @@ export function ProductList() {
       <AsideMenu productQuantity={quantity} />
       <section className="w-full flex mt-8">
         <div className="flex flex-wrap justify-center w-40 gap-12 flex-grow px-8 flex-1">
-          {productFilter.length <= 0 && products?.map((product) => (
+          {!isPending && productFilter.length <= 0 && products?.map((product) => (
             <div className='w-[300px]' key={product.id}>
               <CardProduct image={product.image} name={product.name} price={product.price} id={product.id} />
             </div>
@@ -34,6 +36,12 @@ export function ProductList() {
           {productFilter.length > 0 && productFilter?.map((product) => (
             <div className='w-[350px]' key={product.id}>
               <CardProduct image={product.image} name={product.name} price={product.price} id={product.id} />
+            </div>
+          ))}
+
+          {isPending && skeletonCards.map((product) => (
+            <div className='w-[350px]' key={product}>
+              <SkeletonCard />
             </div>
           ))}
         </div>
