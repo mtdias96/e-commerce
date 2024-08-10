d1de4ebe0c9125a361282ca5c742e4836259fffe
mtdias96 <matheusdias.front96@gmail.com>
2024-08-10 11:54:20 -0300

    bug: baseURL incorrect

--- src/app/services/httpClient.ts
+++ src/app/services/httpClient.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-  // baseURL: 'https://ecommerce-api-jo6k.onrender.com'
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://ecommerce-api-jo6k.onrender.com'
 })
 
 export default httpClient
