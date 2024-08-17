c7170bfe98c221d583274b3619d5c363d89ac3e5
mtdias96 <matheusdias.front96@gmail.com>
2024-08-16 21:34:57 -0300

    fit: responsivo cart card step

--- src/view/pages/ProductCheckout/components/steps/CartStep/CartCardStep.tsx
+++ src/view/pages/ProductCheckout/components/steps/CartStep/CartCardStep.tsx
@@ -1,6 +1,6 @@
 import { FaMinus, FaPlus } from "react-icons/fa6";
 import { FiTrash } from "react-icons/fi";
-
+import { useMediaQuery } from "react-responsive";
 import { formatCurrency } from "../../../../../../app/utils/formatCurrency";
 import imgGroup from "../../../../../../app/utils/imgGroup";
 
@@ -10,53 +10,112 @@ type CardCartProps = {
   price: number;
   size: string;
   image: number;
+  quantity: number;
   className?: string;
 };
 
-export function CartCardStep({ image, price, size, id, name }: CardCartProps) {
+export function CartCardStep({
+  name,
+  image,
+  price,
+  size,
+  quantity = 1,
+}: CardCartProps) {
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 850px)",
+  });
+
   return (
-    <div>
-      <div className="mt-5 flex justify-between w-full border-1 border-b  pt-4 h-36">
-        <div className="flex gap-4">
-          <div className="max-w-[84px] max-h-[84px]">
-            <img
-              src={imgGroup[5]}
-              alt="Product Image"
-              className="min-w-[5px] min-h-[5px] rounded-md flex-1"
-            />
-          </div>
+    <div className="">
+      {!isDesktop && (
+        <div className="mt-5 flex justify-between w-full pt-4 h-36">
+          <div className="flex gap-4">
+            <div className="max-w-[84px] max-h-[84px]">
+              <img
+                src={imgGroup[image]}
+                alt="Product Image"
+                className="min-w-[5px] min-h-[5px] rounded-md flex-1"
+              />
+            </div>
 
-          <div className="flex flex-col gap-1">
-            <h3 className=" md:text-lg font-bold uppercase text-zinc-700 tracking-[-0.02px] leading-tight">
-              Tênis nike air force 1 masculino - <span>{48}</span>
-            </h3>
+            <div className="flex flex-col gap-3.5 text-sm">
+              <h3 className="font-bold uppercase text-zinc-700 tracking-[-0.02px] leading-tight">
+                TÊNIS {name} - <span className="">{size}</span>
+              </h3>
 
-            <span className="text-sm md:text-lg font-bold text-zinc-600">
-              {formatCurrency(1200)}
-            </span>
+              <span className="text-sm md:text-lg font-bold text-zinc-600">
+                {formatCurrency(price)}
+              </span>
 
-            <div className="flex md:mt-0 ">
-              <div className="flex items-center justify-between gap-3 ">
-                <button className="text-lg md:text-xl text-gray-500 focus:outline-none">
-                  <FaMinus className="h-3 w-3 text-zinc-800" />
-                </button>
-                <span className="text-sm  mx-4 text-zinc-800 font-bold">
-                  {8}
-                </span>
-                <button className="text-lg md:text-xl text-gray-500 focus:outline-none">
-                  <FaPlus className="h-3 w-3 text-zinc-800" />
-                </button>
+              <div className="flex md:mt-0 ">
+                <div className="flex items-center justify-between gap-3 ">
+                  <button className="text-lg md:text-xl text-gray-500 focus:outline-none">
+                    <FaMinus className="h-3 w-3 text-zinc-800" />
+                  </button>
+                  <span className="text-sm  mx-4 text-zinc-800 font-bold">
+                    {8}
+                  </span>
+                  <button className="text-lg md:text-xl text-gray-500 focus:outline-none">
+                    <FaPlus className="h-3 w-3 text-zinc-800" />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div>
-          <button className="ml-4  focus:outline-none">
-            <FiTrash className="w-4 h-4" />
-          </button>
+          <div>
+            <button className="ml-4 focus:outline-none">
+              <FiTrash className="w-4 h-4" />
+            </button>
+          </div>
         </div>
-      </div>
+      )}
+
+      {isDesktop && (
+        <div className="flex items-start justify-between gap-4">
+          <div className="flex gap-4">
+            <div className="w-20 h-20">
+              <img
+                src={imgGroup[image]}
+                alt="Product Image"
+                className="w-full h-full object-cover rounded-md"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <h3 className="text-lg font-bold uppercase text-zinc-700">
+                TÊNIS {name} - <span className="">{size}</span>
+              </h3>
+              <span className="text-sm">Produto fornecido por NEWBOOT </span>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center w-96 gap-4 px-6 relative">
+            <span className="text-sm font-bold text-zinc-600">
+              {formatCurrency(price)}
+            </span>
+
+            <div className="flex items-center ">
+              <button className="text-lg text-gray-500 focus:outline-none">
+                <FaMinus className="h-4 w-4 text-zinc-800" />
+              </button>
+              <span className="text-sm mx-4 text-zinc-800 font-bold">
+                {quantity}
+              </span>
+              <button className="text-lg text-gray-500 focus:outline-none">
+                <FaPlus className="h-4 w-4 text-zinc-800" />
+              </button>
+            </div>
+
+            <span className="text-sm font-bold text-zinc-600">
+              {formatCurrency(price * quantity)}
+            </span>
+            <button className="ml-4 focus:outline-none absolute right-[-10px]">
+              <FiTrash className="w-4 h-4" />
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/view/pages/ProductCheckout/components/steps/CartStep/index.tsx
+++ src/view/pages/ProductCheckout/components/steps/CartStep/index.tsx
@@ -1,13 +1,47 @@
+import { useMediaQuery } from "react-responsive";
+import { useCart } from "../../../../../../app/hooks/useCart";
 import { CartCardStep } from "./CartCardStep";
 
 export function CartStep() {
+  const isDesktop = useMediaQuery({
+    query: "(min-width: 850px)",
+  });
+  const { productCart } = useCart();
+
   return (
-    <div className="">
-      <h2 className="font-bold text-2xl">Seu carrinho</h2>
+    <div className="mx-8">
+      <h2 className="font-bold text-2xl ">Seu carrinho</h2>
 
       <section className="mt-6">
-        <span className="text-zinc-500 text-sm">Produtos</span>
-        <CartCardStep />
+        <div className="flex justify-between items-center w-full">
+          <span className="text-zinc-500 text-sm flex-1">Produtos</span>
+
+          {isDesktop && (
+            <div className="flex justify-between w-96 gap-4">
+              <span className="text-zinc-500 text-sm text-center flex-1">
+                Preço Unid.
+              </span>
+              <span className="text-zinc-500 text-sm text-center flex-1">
+                Qtd.
+              </span>
+              <span className="text-zinc-500 text-sm text-center flex-1">
+                Total
+              </span>
+            </div>
+          )}
+        </div>
+
+        {productCart.map(({ image, name, price, id, size, quantity }) => (
+          <div key={id} className="  border-b  border-zinc-200 py-6 mt-4">
+            <CartCardStep
+              image={image[0]}
+              name={name}
+              price={price}
+              size={size}
+              quantity={quantity}
+            />
+          </div>
+        ))}
       </section>
     </div>
   );
