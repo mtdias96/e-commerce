aeeecdc0a7a28be8d129ef1acafa5244206726a1
mtdias96 <matheusdias.front96@gmail.com>
2024-08-30 14:57:23 -0300

    new url ngrokl

--- src/app/services/httpClient.ts
+++ src/app/services/httpClient.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-  baseURL: 'https://e46d-2804-431-cfd9-4a44-4980-7f4c-24b8-7c6a.ngrok-free.app',
+  baseURL: 'https://e9ae-2804-431-cfd9-4a44-e17b-ec89-cdf3-ffb7.ngrok-free.app',
   headers: {
     'ngrok-skip-browser-warning': 'true'
   }
+
 })
 
 export default httpClient
