637f1bc63f380a8eee86e9c4aba052568c472022
mtdias96 <matheusdias.front96@gmail.com>
2024-08-31 16:26:44 -0300

    fix: urln

--- src/app/services/httpClient.ts
+++ src/app/services/httpClient.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-  // baseURL: 'https://e9ae-2804-431-cfd9-4a44-e17b-ec89-cdf3-ffb7.ngrok-free.app',
-  // headers: {
-  //   'ngrok-skip-browser-warning': 'true'
-  // }
-
-  baseURL: 'http://localhost:8080'
+  baseURL: 'https://e9ae-2804-431-cfd9-4a44-e17b-ec89-cdf3-ffb7.ngrok-free.app',
+  headers: {
+    'ngrok-skip-browser-warning': 'true'
+  }
 })
 
 export default httpClient
