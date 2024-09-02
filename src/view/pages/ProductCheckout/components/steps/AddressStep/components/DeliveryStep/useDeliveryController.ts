cb2803dcfb78944be15ef04fc6ad726a719e952d
mtdias96 <matheusdias.front96@gmail.com>
2024-09-02 20:30:49 -0300

    feat: address search by zip codet

--- src/view/pages/ProductCheckout/components/steps/AddressStep/components/DeliveryStep/useDeliveryController.ts
+++ src/view/pages/ProductCheckout/components/steps/AddressStep/components/DeliveryStep/useDeliveryController.ts
@@ -1,8 +1,9 @@
+import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
+
+import { cepAddress } from "../../../../../../../../app/services/CepService/cepAddress";
 import { sleep } from "../../../../../../../../app/utils/sleep";
 import { useStepper } from "../StepperAddress/useStepperAddress";
-
-export function useDeliveryController(){
 
 interface IPersonalData {
   cep: string;
@@ -15,25 +16,49 @@ interface IPersonalData {
   recipient: string;
 }
 
+export function useDeliveryController() {
   const {
     register,
     handleSubmit: submit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<IPersonalData>({ shouldFocusError: true, mode: "onChange" });
-
   const { nextStep } = useStepper();
+
+  const { mutateAsync, isPending, isSuccess } = useMutation({
+    mutationFn: async (data: string) => {
+      await sleep(1000);
+      return cepAddress(data);
+    },
+  });
 
   const handleSubmit = submit(
     async (data) => {
-      await sleep(1000);
-      console.log({data});
-      nextStep();
+      try {
+        const { bairro, logradouro, localidade, uf } = await mutateAsync(data.cep);
+        setValue('address', logradouro);
+        setValue('zone', bairro);
+        setValue('city', localidade);
+        setValue('state', uf);
+
+
+
+      } catch (error) {
+        console.error('Erro ao buscar o endereço:', error);
+      }
     },
     (errors) => {
       console.log({ errors });
     }
   );
 
-  return { handleSubmit, errors, isSubmitting, register, nextStep };
+  return {
+    handleSubmit,
+    errors,
+    isSubmitting,
+    register,
+    nextStep,
+    isPending,
+    isSuccess
+  };
 }
-
