48c38bce026b8d0e62a584f777e48d148b13dcfc
mtdias96 <matheusdias.front96@gmail.com>
2024-10-02 22:20:20 -0300

    feat: edit address

--- src/view/pages/ProductCheckout/components/steps/AddressStep/components/DeliveryStep/boxAddress.tsx
+++ src/view/pages/ProductCheckout/components/steps/AddressStep/components/DeliveryStep/boxAddress.tsx
@@ -3,14 +3,18 @@ interface BoxProps {
   zone?: string;
   city?: string;
   state?: string;
+  isOpen: () => void;
 }
 
-export function BoxAdress({ addess, city, state, zone }: BoxProps) {
+export function BoxAdress({ addess, city, state, zone, isOpen }: BoxProps) {
   return (
     <div className="w-full p-4 h-24 border mt-8 text-sm">
       <div className="flex justify-between mb-0.5">
         <h2 className="font-bold tracking-[0.5px] flex">Endereço de Entrega</h2>
-        <button className="uppercase font-bold text-xs border-b border-black">
+        <button
+          onClick={isOpen}
+          className="uppercase font-bold text-xs border-b border-black"
+        >
           Editar
         </button>
       </div>
