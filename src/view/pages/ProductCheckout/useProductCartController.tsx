4f34220fa2620f4f92d49ebc80b610f565185d7b
mtdias96 <matheusdias.front96@gmail.com>
2024-11-13 21:39:35 -0300

    using useShallow

--- src/view/pages/ProductCheckout/useProductCartController.tsx
+++ src/view/pages/ProductCheckout/useProductCartController.tsx
@@ -1,13 +1,15 @@
 import { useMemo } from "react";
 import { useDiscount } from "../../../app/hooks/useDiscount";
 import { useStore } from "../../../app/store";
+import { useShallow } from "zustand/shallow";
 
 export function useProductCartController() {
-  const { productCart, totalPrice } = useStore((state) => ({
-    productCart: state.cart.productCart,
-    totalPrice: state.cart.totalPrice,
-  }));
-
+  const { productCart, totalPrice } = useStore(
+    useShallow((state) => ({
+      productCart: state.cart.productCart,
+      totalPrice: state.cart.totalPrice,
+    }))
+  );
   const { valueDiscount, discountedValue } = useDiscount(totalPrice);
 
   return useMemo(
